b323f6887060847d711d626692f5a161e0f0481c
prana <pranaytb777@gmail.com>
2026-02-18 12:56:26 +0000

    Integrate media fetching functionality: Added API integration for fetching photos and videos using Unsplash and Pexels APIs. Updated dependencies in `package.json` and configured Tailwind CSS in the Vite setup. Removed unused CSS styles and added new components for search and results display.

--- redux_project/src/App.jsx
+++ redux_project/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { fetchPhotos, fetchVideos } from "./api/mediaAPI.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  async function getPhotos() {
+    const result = await fetchPhotos("cat");
+    console.log(result);
+  }
+
+  async function getVideos() {
+    const result = await fetchVideos("cat");
+    console.log(result);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>Redux Project.</h1>
+        <button
+          className="border bg-amber-500 me-2 p-2 rounded-md"
+          onClick={getPhotos}
+        >
+          get photos
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          className="border bg-amber-500 me-2 p-2 rounded-md"
+          onClick={getVideos}
+        >
+          get videos
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
